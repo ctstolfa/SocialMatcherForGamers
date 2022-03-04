c451f9c1bfe96b617e2969c3461d636b3ef8720f
Colin Stolfa <ctstolfa@uwm.edu>
2022-03-04 10:05:41 -0600

    Connected User Profiles to Search Updated Search to Work with User Model instead of Account

--- SMG/smg_app/urls.py
+++ SMG/smg_app/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login', views.login, name="login"),
+    path('loginPage', views.loginPage, name="loginPage"),
     path('register', views.register, name="register"),
     path("logout", views.logout, name="logout"),
+    path("profile/(?P<username>\w+)", views.profile, name="profile"),
     #path("signup", views.signup, name="signup"),
     path('search', views.search, name='search'),
 ]
--- SMG/smg_app/views.py
+++ SMG/smg_app/views.py
@@ -1,7 +1,8 @@
-
+from django.http import Http404
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout as logout_user
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Account
 
@@ -19,14 +20,14 @@ def register(request):
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             messages.success(request, "Registration successful!")
-            return redirect('login')
+            return redirect('loginPage')
     else:
         form = UserCreationForm()
 
     return render(request, 'signUp.html', {'form': form, })
 
 
-def login(request):
+def loginPage(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -36,32 +37,50 @@ def login(request):
         if user is not None:
             login(request, user)
             uName = user.username
-            return render(request, "loginPage.html", {'uName': uName})
+            return render(request, "search.html", {'uName': uName})
         else:
             # messages.error(request, 'Bad Credential')
-            return redirect('signup')
+            return redirect('register')
     return render(request, 'loginPage.html')
 
 
 def logout(request):
     logout_user(request)
-    return redirect("login")
+    return redirect("loginPage")
 
 
 def search(request):
     if request.method == "POST":
         searched = request.POST['searched']
-        users = Account.objects.filter(name__contains=searched)
+        users = User.objects.all().filter(username__contains=searched).filter(is_staff=0)
         return render(request, 'search.html', {'searched': searched,
                                                'users': users,})
     else:
         return render(request, 'search.html', {})
 
 
-def profile(request):
-    if request.method == "POST":
-        user = request.POST['user']
-        user_profile = Account.objects.filter(name__exact=user)
-        return render(request, 'profile.html', {'user':user, 'user_profile': user_profile})
-    else:
-        return render(request, 'profile.html', {})
+def profile (request, username):
+    # If no such user exists raise 404
+    try:
+        user = User.objects.get(username=username)
+    except:
+        raise Http404
+
+    # Flag that determines if we should show editable elements in template
+    editable = False
+    # Handling non authenticated user for obvious reasons
+    if request.user.is_authenticated() and request.user == user:
+        editable = True
+
+    context = locals()
+    template = 'profile.html'
+    return render (request, template, context)
+
+
+#def profile(request):
+#    if request.method == "POST":
+#        user = request.POST['user']
+#        user_profile = Account.objects.filter(name__exact=user)
+#        return render(request, 'profile.html', {'user':user, 'user_profile': user_profile})
+#    else:
+#        return render(request, 'profile.html', {})
