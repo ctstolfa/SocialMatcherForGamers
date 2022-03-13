308b45a60da4727124cdb01f845186fc4e65950a
aaron <azewudie@uwm.edu>
2022-03-13 15:16:06 -0500

    added a static file and bootsrap

--- SMG/smg/settings.py
+++ SMG/smg/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'smg_app',
 
+
 ]
 
 MIDDLEWARE = [
@@ -114,6 +115,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/admin'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+# STATIC_ROOT = os.path.join(BASE_DIR,'static'),
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'CSS'),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
