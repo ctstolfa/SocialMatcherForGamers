25abe2a0f08538a620752368f250ea01b3560640
aaron <azewudie@uwm.edu>
2022-03-16 12:31:27 -0500

    added style for register and search page

--- SMG/smg_app/urls.py
+++ SMG/smg_app/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     # path('', views.home, name='home'),
+    # path("signup", views.signup, name="signup"),
     path('', views.loginPage, name="LoginPage"),
     path('register', views.register, name="register"),
     path("logout", views.logout, name="logout"),
     path("profile/<username>", views.profile, name="profile"),
-    #path("signup", views.signup, name="signup"),
+
     path('search', views.search, name='search'),
 ]
