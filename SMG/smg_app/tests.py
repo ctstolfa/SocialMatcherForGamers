79f545ddb310aacd9915cdeee261f9fe95ba2ae1
UminekoVII <95431561+UminekoVII@users.noreply.github.com>
2022-04-21 17:41:59 -0500

    Login and Register tests

--- SMG/smg_app/tests.py
+++ SMG/smg_app/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from .models import Account
 from django.test import TestCase
 User = get_user_model()
 
@@ -15,6 +16,14 @@ class UserLogin(TestCase):
         user_a.set_password(user_a_pw)
         self.user_a = user_a
 
+        account_a = Account('UserTest')
+        account_a.time = ('Morning', 'Midnight')
+        account_a.role = 1
+        account_a.days = 'Wednesday'
+        account_a.gameStyle = 'Competitive'
+        account_a.genres = ('RPG', 'Horror', 'Survival')
+        self.account_a = account_a
+
     def test_user_exists(self):
         user_count = User.objects.all().count()
         self.assertEqual(user_count, 1)
@@ -22,3 +31,15 @@ class UserLogin(TestCase):
 
     def test_user_password(self):
         self.assertTrue(self.user_a.check_password("TestingThisPassword1@"))
+
+    def test_account_exists(self):
+        account_count = User.objects.all().count()
+        self.assertEqual(account_count, 1)
+        self.assertNotEqual(account_count, 0)
+
+    def test_account_attributes(self):
+        self.assertEqual(self.account_a.time, ('Morning', 'Midnight'))
+        self.assertEqual(self.account_a.role, 1)
+        self.assertEqual(self.account_a.days, 'Wednesday')
+        self.assertEqual(self.account_a.gameStyle, 'Competitive')
+        self.assertEqual(self.account_a.genres, ('RPG', 'Horror', 'Survival'))
